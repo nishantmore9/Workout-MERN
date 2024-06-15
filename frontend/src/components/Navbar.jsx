1c5443037bab0ec802761e797260f79c99188350
Nishant More <nishantmore228@gmail.com>
2024-06-15 14:22:38 +0530

    Add UI changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -11,16 +11,16 @@ const Navbar = () => {
     logout()
   }
   return (
-    <nav className='h-20 flex items-center max-w-7xl m-auto py-4 px-10 justify-between shadow-md'>
-      <div className='flex justify-around items-center gap-5'>
-        <img src="https://img.icons8.com/?size=100&id=jNKypI1BeVDU&format=png&color=000000" alt="img" height={50} width={50} />
-        <Link to = "/"><h1 className='font-bold text-4xl font-mono'>Workout Buddy</h1></Link>
+    <nav className='h-20 flex items-center max-w-7xl m-auto py-4 px-10 justify-between'>
+      <div className='flex justify-around items-center gap-5 cursor-pointer'>
+        <img src="https://img.icons8.com/?size=100&id=jNKypI1BeVDU&format=png&color=000000" alt="img" height={35} width={35} />
+        <Link to = "/"><h1 className='font-bold text-4xl font-mono'>Workout<span className='text-green-500 font-bold'>B</span>uddy</h1></Link>
       </div>
       <div>
         {user && (
-          <div className='flex gap-3'>
+          <div className='flex gap-5 items-center'>
             <div>{user.email}</div>
-            <button onClick={handleSubmit}>Log out</button>
+            <button onClick={handleSubmit} className='text-green-500 uppercase border-2 border-green-500 bg-green-50 px-2 py-1 font-bold rounded-sm'>Log out</button>
           </div>
         )}
         {!user && (
--- frontend/src/components/WorkoutCard.jsx
+++ frontend/src/components/WorkoutCard.jsx
@@ -35,7 +35,9 @@ const WorkoutCard = ({_id , title, load, reps, createdAt}) => {
         <p className='text-gray-500'>{formatDistanceToNow(new Date(createdAt), {addSuffix : true})}</p>
       </div>
       <div>
-        <button onClick={handleClick} className='uppercase text-gray-600 bg-slate-100 px-2 py-1 rounded-lg'>Delete</button>
+        <button onClick={handleClick} className='uppercase text-gray-600 bg-slate-100 px-2 py-2 rounded-full'>
+          <img src="https://img.icons8.com/?size=100&id=85081&format=png&color=000000" alt="delete" width={20} height={20}/>
+        </button>
       </div>
     </div>
   )
